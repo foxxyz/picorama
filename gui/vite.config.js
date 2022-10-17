6b768df517a68a91f8cd03e6e5c9f914839b376d
foxxyz <foxxyz@gmail.com>
2022-10-16 17:13:18 -0700

    feat(gui): serve images from disk using custom plugin in dev mode

--- gui/vite.config.js
+++ gui/vite.config.js
@@ -1,8 +1,26 @@
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import sirv from 'sirv'
+
+// Middleware to serve a static photos directory from accessing /photos/...
+function servePhotosMiddleware(directory) {
+    const serve = sirv(directory)
+    return function(req, res, next) {
+        req.url = req.url.replace('/photos/', '/')
+        serve(req, res, next)
+    }
+}
+
+// Plugin for serving static images from ../server/thumbs under http://.../photos/..
+const serveImages = () => ({
+    name: 'serve-images',
+    configureServer(server) {
+        server.middlewares.use(servePhotosMiddleware('../server/thumbs'))
+    }
+})
 
 export default {
-    plugins: [vue()],
+    plugins: [vue(), serveImages()],
     resolve: {
         alias: {
             '@': resolve(__dirname, './src'),
